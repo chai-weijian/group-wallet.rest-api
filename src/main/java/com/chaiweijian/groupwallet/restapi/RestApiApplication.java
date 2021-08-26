63c4152ade4e6cc76ca06295d2e63c4ed9e9de87
Chai Wei Jian <chaiweijian@protonmail.com>
2021-08-26 09:46:45 +0800

    chore: add cors

--- src/main/java/com/chaiweijian/groupwallet/restapi/RestApiApplication.java
+++ src/main/java/com/chaiweijian/groupwallet/restapi/RestApiApplication.java
@@ -16,6 +16,9 @@ package com.chaiweijian.groupwallet.restapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class RestApiApplication {
@@ -24,4 +27,15 @@ public class RestApiApplication {
         SpringApplication.run(RestApiApplication.class, args);
     }
 
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:3000")
+                        .allowedMethods("*");
+            }
+        };
+    }
 }
